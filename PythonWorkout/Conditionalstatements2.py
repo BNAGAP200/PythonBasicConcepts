3f5fa01b84682c0b8b3f39127b86b8efbddda28d
BNAGAP200 <balajinagappan@hotmail.com>
2024-04-12 23:58:05 +0100

    New files and concepts are added

---
+++ PythonWorkout/Conditionalstatements2.py
@@ -0,0 +1,14 @@
+
+def check_values():
+    a = 10
+    b = 10
+
+    if a == 10 and b == 10 and  a + b == 10:  #Use or ,And for different scenerios
+        return True
+    else:
+        return False
+result = check_values()
+print(result)
+
+
+
---
+++ PythonWorkout/Newworkoutfiles.py
@@ -0,0 +1,29 @@
+try:
+    user_input = int(input("Enter a number: "))
+    user_input1 = int(input("Enter another number: "))
+    c = user_input / user_input1
+    print( c)
+except ValueError:
+    print("Please enter valid integers.")
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+#index error
+
+try:
+    A= [1,2,5,7,8,]
+    b={1,5,4,8,7}
+    print(A[10])
+
+except IndexError:
+    print("out of zeroindex")
+
+
+
+#Key error#
+try:
+    Marks = {'Maths' : 85,'Science' :92,'English' :88 }
+    print(Marks['Art'])
+
+except KeyError:
+    print("Showing key error")
